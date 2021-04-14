97f6b0b223f4eb4accb3d538d4373610ff13934e
LukeSutor <lukesutor@gmail.com>
2021-04-14 11:06:12 -0400

    update index.js for production

--- index.js
+++ index.js
@@ -15,22 +15,6 @@ app.use('/walmartScraper', walmartScraper)
 app.use('/ebayScraper', ebayScraper)
 app.use('/targetScraper', targetScraper)
 
-
-
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('../web/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'web', 'build', 'index.html'));
-  });
-}
-
-
-
-
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
